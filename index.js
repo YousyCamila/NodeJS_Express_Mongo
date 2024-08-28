eda9fd14f64f4175d5c24f85c6a9a9b9c02794bf
Camilla Guerra Rincon <danna_cguerra@soy.sena.edu.co>
2024-08-28 15:43:50 -0500

    Estructurando el proyecto API REST. Index, modelos y controladores

---
+++ index.js
@@ -0,0 +1,26 @@
+const usuarios = require('./controllers/usuarios');
+const cursos = require('./controllers/cursos');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Conexion a la base de datos MongoDB
+mongoose.connect('mongodb://localhost:27017/userscoursesdb')
+    .then(() => console.log('Conectado a MongoDB...'))
+    .catch(err => console.error('No se pudo conectar con MongoDB:', err));
+
+// Middleware 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//end points (recursos)
+app.use('/api/usuarios', usuarios);
+app.use('/api/cursos', cursos);
+
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('API REST Ok, y ejecutándose...');
+});
